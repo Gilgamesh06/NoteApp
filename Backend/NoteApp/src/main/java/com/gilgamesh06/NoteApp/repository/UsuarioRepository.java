e86c7ea37f92fb45024a118a05a30e73482929e1
Solus <gilgamesh06lg@gmail.com>
2025-08-27 15:34:26 -0500

    Se creo la capa UsuarioRepositorio en la capa repository

--- Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/UsuarioRepository.java
+++ Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/UsuarioRepository.java
@@ -1,9 +1,20 @@
 package com.gilgamesh06.NoteApp.repository;
 
+import com.gilgamesh06.NoteApp.model.entity.Persona;
 import com.gilgamesh06.NoteApp.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    @Override
+    Optional<Usuario> findById(Long aLong);
+
+    Optional<Usuario> save(Usuario usuario);
+
+    Optional<Usuario> findByPersona(Persona persona);
+
 }
