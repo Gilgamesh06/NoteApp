569f6f6a16de15fb4256ba36fc6119ec84416624
Solus <gilgamesh06lg@gmail.com>
2025-08-27 15:33:46 -0500

    Se creo la clase PersonaRepository en la capa repository

--- Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/PersonaRepository.java
+++ Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/PersonaRepository.java
@@ -4,7 +4,15 @@ import com.gilgamesh06.NoteApp.model.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona,Long> {
+
+    Optional<Persona> save(Persona persona);
+
+    Optional<Persona> findById(Long id);
+
+    Optional<Persona> find(Persona persona);
 }
 
