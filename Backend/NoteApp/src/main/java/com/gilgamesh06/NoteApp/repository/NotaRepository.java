c09393968f998fcbaf6162bb2e74b63bcd4873e4
Solus <gilgamesh06lg@gmail.com>
2025-09-12 10:57:51 -0500

    Se actualizo la clase NotaRepository se agregaron metodos para retornar notas Paginadas

--- Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/NotaRepository.java
+++ Backend/NoteApp/src/main/java/com/gilgamesh06/NoteApp/repository/NotaRepository.java
@@ -1,8 +1,10 @@
 package com.gilgamesh06.NoteApp.repository;
 
 import com.gilgamesh06.NoteApp.model.entity.Nota;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -10,7 +12,10 @@ import java.util.Optional;
 public interface NotaRepository extends JpaRepository<Nota,Long> {
 
     Optional<Nota> findById(Long id);
-    
+
+    Page<Nota> findAll(Pageable pageable);
+
+    Page<Nota> findByEstado(boolean estado, Pageable pageable);
 
     void deleteById(Long id);
 }
