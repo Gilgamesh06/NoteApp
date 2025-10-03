7ad7f58adbf7baa72e5bc2edcd839b149bea219c
Solus <gilgamesh06lg@gmail.com>
2025-10-03 18:06:26 -0500

    Se creo el proyecto base del frontend

---
+++ Frontend/js/model/user.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(nickname,password){
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    getNickname(){
+        return this.nickname;
+    }
+
+    setNickname(nickname){
+        this.nickname = nickname;
+    }
+
+    setPassword(password){
+        this.password = password;
+    }
+}
