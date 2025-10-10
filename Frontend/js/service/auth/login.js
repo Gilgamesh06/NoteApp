e105728a52f3f2f99e1fe1b797923e1f32964065
Solus <gilgamesh06lg@gmail.com>
2025-10-09 19:05:17 -0500

    se actualizo el script login.js

---
+++ Frontend/js/service/auth/login.js
@@ -0,0 +1,37 @@
+import { UserDTO } from "../../dto/userDTO.js";
+import { sendAuth } from "../../controller/authController.js"; 
+import { URL } from "../../const/const.js";
+import { validType , validPassword } from "../../valid/validRegister.js";
+
+// Manejador del login
+const fromLogin =  document.getElementById('login');
+
+fromLogin.addEventListener('submit', async function (event){
+    // Previene que el formulario se envie de forma tradicional
+    event.preventDefault(); 
+    
+    // Capturando los valores ingresado por el usuario
+    const nickname = document.getElementById('nickname').value;
+    const password = document.getElementById('password').value;
+
+    // Validation
+    try {
+        validType(nickname);
+        validPassword(password);
+        
+    }catch(error){
+        console.log(error);
+    }
+    
+    // Crea el DTO
+    const user = new UserDTO(nickname,password);
+
+    try{
+        // Funcion para enviar datos al backend
+        const result = await sendAuth(user, `${URL}/login`);
+        console.log("Usuario logeado:", result); 
+    }catch(error){
+        console.log("Error al logearse: ", error);
+    }
+});
+
