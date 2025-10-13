7bb7d287e5371cbe6d225e11e367d27f95eac119
Solus <gilgamesh06lg@gmail.com>
2025-10-13 16:17:04 -0500

    Se separaron las querys a los enpoints del backend en un solo archivo

---
+++ Frontend/js/controller/querys.js
@@ -0,0 +1,51 @@
+
+export async function getElements(token, URL){
+    const res = await fetch(`${URL}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}` 
+        }
+    });
+    if (!res.ok) throw new Error(`HTTP: ${res.status}`);
+
+    return await res.json();
+}
+
+export async function sendAuth(body, URL){
+    const res = await fetch( `${URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    if (!res.ok) throw new Error(`HTTP: ${res.status}`);
+    
+    return await res.json();
+}
+
+export async function sendNote(token ,body, URL){
+    const res = await fetch( `${URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`, 
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    if (!res.ok) throw new Error(`HTTP: ${res.status}`);
+    
+    return await res.json();
+}
+
+export async function getHTML(URL){
+    const res = await fetch(`${URL}`);
+    
+    if (!res.ok) throw new Error('Error al cargar el arhivo: '+ res.statusText );
+    
+    return res.text(); // Convierte la respuesta en texto
+
+}
