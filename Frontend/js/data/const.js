36b96c8dd32b6f85498b73b8098d00a7b0636430
Solus <gilgamesh06lg@gmail.com>
2025-10-14 21:51:04 -0500

    Se modifico el fichero const.js se agregaron nuevas URLS

--- Frontend/js/data/const.js
+++ Frontend/js/data/const.js
@@ -1,3 +1,13 @@
-
+// URLS Backend
 export const URL_AUTH = "http://localhost:8081/auth";
 export const URL_NOTES = "http://localhost:8081/note_app/v1/note";
+
+// URLS HTML (todas relativas a index.html)
+export const URL_BAR = '/html/componets/home/bar.html';
+export const URL_HOME = '/html/componets/home/home.html';
+export const URL_LOGIN = '/html/componets/auth/login.html';
+export const URL_REGISTER = '/html/componets/auth/register.html';
+export const URL_LISTOPTIONS = '/html/componets/note/list-note.html';
+export const URL_BARNOTE = '/html/componets/note/bar-note.html';
+export const URL_CREATENOTE = '/html/componets/note/create-note.html';
+export const URL_BUTTONS = '/html/componets/note/buttons.html';
