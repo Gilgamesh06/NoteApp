7d92ebd553b0f1b311ced4de49bbc4c7d00b4b35
Solus <gilgamesh06lg@gmail.com>
2025-10-17 20:38:55 -0500

    Se actualizo main.js ahora tiene las funcionalidades que tenia methodMain.js

--- Frontend/js/main/main.js
+++ Frontend/js/main/main.js
@@ -1,75 +1,145 @@
+import { getHTML } from "../controller/querys.js";
 import { URL_BAR, URL_REGISTER, URL_HOME } from "../data/const.js";
 import { userRegister } from "../service/auth/register/register.js";
-import { cargarHTML, handleClickCargarHTML } from "./methodMain.js";
 import { cargarLoginAndEventSend } from "../service/auth/login/login.js";
 
-const header = document.getElementById('header');
-const bar = 'bar';
+export function containderDiv(){
+    return {
+        header: document.getElementById('header'),
+        container: document.getElementById('container'),
+        options: document.getElementById('options')
+    }
+}
 
-// Contenedor 
-const container = document.getElementById('container');
-const tituloHome = 'tituloHome';
-const funcionalidades = 'funcionalidades';
+export function barMenus(){
+    return {
+        bar: 'bar',
+        barNote: 'barNote',
+        listNote: 'listNote'
+    }
+}
 
-// Carga el menu de opciones principal
-await cargarHTML(header, bar, URL_BAR);
+function barIncial(){
+    return {
+        loginLi: document.getElementById('login'),
+        registerLi: document.getElementById('register'),
+        homeLi: document.getElementById('home')
+    }
+}
 
-// Carga el menu de opciones principal
-await cargarHTML(container, tituloHome, URL_HOME);
+function homeData(){
+    return {
+        tituloHome: 'tituloHome',
+        funcionalidades: 'funcionalidades'
+    }
+}
 
-// Carga el menu de opciones principal
-await cargarHTML(container, funcionalidades, URL_HOME, false);
 
-// Elementos de la lista que son cargados en el header 
-const loginLi = document.getElementById('login'); 
-const registerLi = document.getElementById('register');
-const homeLi = document.getElementById('home');
-
-if(registerLi){
-    registerLi.addEventListener('click', async () => {
-
-        try{
-            
-            await handleClickCargarHTML(container,`registerFrom`, URL_REGISTER);
-            // Espera a que el formulario se inyecte para añadir el envento
-            
-            // Manejador del formulario
-            const fromResgister = document.getElementById('registerFrom');
-            
-            if(fromResgister){
-                // Evento para enviar formulario para logearse
-                fromResgister.addEventListener('submit', userRegister);
-            }else{
-                console.error("El formulario no se encontro despues de cargar");
-            }  
-
-        }catch(error){
-            console.error("Error al cargar el formulario de Register:", error);
+export async function cargarHTML(container, id, URL, drop = true) {
+    
+    const parser = new DOMParser(); // Crea un parse de DOM
+    try{
+        const html = await getHTML(URL);
+        const doc = parser.parseFromString(html, 'text/html'); // Convierte el texto HTML en un documento
+        const insertElement = doc.getElementById(id); // Obtiene el elemento a insertar
+        if (insertElement){
+            if(drop){
+                // Limpiar el contenedor antes de agregar nuevas notas
+                container.innerHTML = "";
+            }
+            container.appendChild(insertElement);
         }
-    });
-}
-
-if(loginLi){
-    // Evento que carga el formulario para logearse
-    loginLi.addEventListener('click', async () => {
-        try{
-            await cargarLoginAndEventSend();     
-        }catch(error){
-            console.error("Error al cargar el formulario de Login:", error);
+        else{
+            console.error(`Elemento con id: ${id}, no encontrado en: ${URL} `);
         }
-    });
-}else{
-    console.error("Error al cargar el menu principal")
+    }catch(err){
+        console.log('Hubo un problema con la peticion Fetch:', err);
+    }
 }
 
-if(homeLi){
-    // Evento que carga el home 
-    homeLi.addEventListener('click', async () => {
-        
-        // Carga el menu de opciones principal
-        await cargarHTML(container, tituloHome, URL_HOME);
+async function load() {
+    
+    const { header, container} = containderDiv();
+    const { bar } = barMenus();
+    const { tituloHome, funcionalidades } = homeData();
 
-        // Carga el menu de opciones principal
-        await cargarHTML(container, funcionalidades, URL_HOME, false);
-    })
+    // Carga el menu de opciones principal
+    await cargarHTML(header, bar, URL_BAR);
+    
+    // Carga el menu de opciones principal
+    await cargarHTML(container, tituloHome, URL_HOME);
+    
+    // Carga el menu de opciones principal
+    await cargarHTML(container, funcionalidades, URL_HOME, false);
 }
+
+export async function startPage() {
+    
+    await load();
+
+    const { registerLi, loginLi, homeLi} = barIncial();
+
+    if(registerLi){
+        registerLi.addEventListener('click', async () => {
+    
+            try{
+                
+                await cargarHTML(container,`registerFrom`, URL_REGISTER);
+                // Espera a que el formulario se inyecte para añadir el envento
+                
+                // Manejador del formulario
+                const fromResgister = document.getElementById('registerFrom');
+                
+                if(fromResgister){
+                    // Evento para enviar formulario para logearse
+                    fromResgister.addEventListener('submit', userRegister);
+                }else{
+                    console.error("El formulario no se encontro despues de cargar");
+                }  
+    
+            }catch(error){
+                console.error("Error al cargar el formulario de Register:", error);
+            }
+        });
+    }
+    
+    if(loginLi){
+        // Evento que carga el formulario para logearse
+        loginLi.addEventListener('click', async () => {
+            try{
+                await cargarLoginAndEventSend();     
+            }catch(error){
+                console.error("Error al cargar el formulario de Login:", error);
+            }
+        });
+    }else{
+        console.error("Error al cargar el menu principal")
+    }
+    
+    if(homeLi){
+        // Evento que carga el home 
+        homeLi.addEventListener('click', async () => {
+            
+            try{
+                const {tituloHome, funcionalidades} = homeData();
+                
+                // Carga el menu de opciones principal
+                await cargarHTML(container, tituloHome, URL_HOME);
+    
+                // Carga el menu de opciones principal
+                await cargarHTML(container, funcionalidades, URL_HOME, false);
+
+            }catch(error){
+                console.error("Error al cargar la pagina de inicio", error);
+            }
+        });
+    }
+}
+
+
+try{
+    await startPage();
+}catch(error){
+    console.error("Error al cargar la pagina principal", error);
+}
+
