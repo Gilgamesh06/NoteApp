9611b211a012fb2c00fdb9455be81ec508f18d15
Solus <gilgamesh06lg@gmail.com>
2025-10-17 20:37:09 -0500

    Se modifico login para que cuando el token sea invalido mande al formulario de login

--- Frontend/js/service/auth/login/login.js
+++ Frontend/js/service/auth/login/login.js
@@ -1,20 +1,15 @@
 import { UserDTO } from "../../../dto/userDTO.js";
 import { sendAuth } from "../../../controller/querys.js"; 
-import { URL_AUTH , URL_LOGIN} from "../../../data/const.js";
-import { saveToken , isTokenValid} from "../../../data/saveToken.js";
-import { handleClickCargarInterfazInterna } from "../../note/note.js";
-import { handleClickCargarHTML } from "../../../main/methodMain.js";
+import { URL_AUTH , URL_LOGIN, URL_BAR} from "../../../data/const.js";
+import { saveToken , isTokenValid} from "../../../valid/token/token.js";
+import { cargarInterfazInterna } from "../../note/note.js";
+import { cargarHTML,containderDiv, barMenus, startPage } from "../../../main/main.js";
 
-function containerForData(){
-    return {
-        header: document.getElementById('header'),
-        container: document.getElementById('container')
-    }
-}
 
+// Funcion que me envia los datos del Login From
 async function userLogin(event){
 
-    const { header, container } = containerForData();
+    const { header, container } = containderDiv();
     
     // Previene que el formulario se envie de forma tradicional
     event.preventDefault(); 
@@ -37,7 +32,7 @@ async function userLogin(event){
         if (isTokenValid()) {
             
             container.innerHTML="";
-            await handleClickCargarInterfazInterna(header,container);
+            await cargarInterfazInterna(header,container);
         
         } else {
             console.error("El token recibido no es válido o está expirado");
@@ -47,21 +42,52 @@ async function userLogin(event){
     }
 }
 
+// Funcion que carga el Formulario de Login y su evento de envio
 export async function cargarLoginAndEventSend(){
 
-    const { container } = containerForData();
+    const { container } = containderDiv();
 
-    await handleClickCargarHTML(container,`loginFrom`, URL_LOGIN);
+    await cargarHTML(container,`loginFrom`, URL_LOGIN);
     // Espera a que el formulario se inyecte para añadir el envento
             
     // Manejador del login
     const fromLogin =  document.getElementById('loginFrom');
             
-    if(fromLogin){
-                
+    if(fromLogin){            
         // Evento para enviar formulario para logearse
         fromLogin.addEventListener('submit', userLogin);  
     }else{
         console.error("El formulario no se encontro despues de cargar");
+    }
+}
+
+// Funcion que limpia los contenedores y carga el Formulario de Login y Menu Inicial
+export async function tokenNotValidLoadLoginFrom(){
+    // Carga los div que se van a modificar 
+    const { options , header } = containderDiv();
+    // borra la lista de opciones de notas
+    options.innerHTML = "";
+    // contiene el id de la barra de menu de inicio
+    const { bar } = barMenus();
+    // carga la barra de menu de inicio            
+    await cargarHTML(header,bar, URL_BAR);
+                
+    // Carga el Login y su envento de envio
+    await cargarLoginAndEventSend();     
+}
+
+export async function unLogin() {
+    try{
+        // Eliminar token
+        localStorage.removeItem('authToken');
+        // Limpie el contenedor de Options y Cambie el Header y Container
+        const {options} = containderDiv();
+        // Limpiamos las opciones
+        options.innerHTML = "";
+
+        await startPage();
+
+    }catch(error){
+        console.error("Error al cargar la pagina de inicio", error);
     }
 }
