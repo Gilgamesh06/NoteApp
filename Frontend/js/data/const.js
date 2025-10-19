2c17c1a99781e3e3cb1985ef906ab6afa1285b47
Solus <gilgamesh06lg@gmail.com>
2025-10-18 21:47:25 -0500

    Se agrego las URLs de los nuevos componentes

--- Frontend/js/data/const.js
+++ Frontend/js/data/const.js
@@ -9,5 +9,7 @@ export const URL_LOGIN = '/html/componets/auth/login.html';
 export const URL_REGISTER = '/html/componets/auth/register.html';
 export const URL_OPTIONS = '/html/componets/note/list-note.html';
 export const URL_BARNOTE = '/html/componets/note/bar-note.html';
+export const URL_BAROPTIONS = '/html/componets/note/bar-options.html';
 export const URL_CREATENOTE = '/html/componets/note/create-note.html';
+export const URL_UPDATENOTE = '/html/componets/note/update-note.html';
 export const URL_BUTTONS = '/html/componets/note/buttons.html';
